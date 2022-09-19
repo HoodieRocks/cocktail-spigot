c55096afb1099e28c84d60ef5df0353d3c28bf05
boop-the-dog <aidenhperry@gmail.com>
2022-09-19 14:59:06 -0400

    conversion of commands to commandapi has begun

--- src/main/java/me/cobble/cocktail/cmds/RandCommand.kt
+++ src/main/java/me/cobble/cocktail/cmds/RandCommand.kt
@@ -1,28 +1,37 @@
 package me.cobble.cocktail.cmds
 
+import dev.jorel.commandapi.CommandAPICommand
+import dev.jorel.commandapi.arguments.IntegerArgument
+import dev.jorel.commandapi.arguments.ScoreHolderArgument
+import dev.jorel.commandapi.arguments.StringArgument
+import dev.jorel.commandapi.executors.CommandExecutor
 import me.cobble.cocktail.utils.Color
 import org.bukkit.Bukkit
-import org.bukkit.command.CommandSender
-import org.bukkit.command.defaults.BukkitCommand
 
-class RandCommand : BukkitCommand("rand") {
-    override fun execute(sender: CommandSender, commandLabel: String, args: Array<out String>): Boolean {
-        if (sender.isOp) {
-            if (args.size == 4) {
-                val min = args[0].toInt()
-                val max = args[1].toInt()
-                val player = args[2]
-                val board = Bukkit.getScoreboardManager()!!.mainScoreboard.getObjective(args[3])!!
 
-                board.getScore(player).score = (min..max).random()
-                return true
-            } else {
-                sender.sendMessage("Too few arguments, /random <min> <max> <player> <board>")
-            }
-            return false
-        } else {
-            sender.sendMessage(Color.color("&cNo permission!"))
-            return false
-        }
+class RandCommand {
+
+    init {
+        CommandAPICommand("rand")
+            .withArguments(IntegerArgument("min", Int.MIN_VALUE, Int.MAX_VALUE))
+            .withArguments(IntegerArgument("max", Int.MIN_VALUE, Int.MAX_VALUE))
+            .withArguments(StringArgument("name"))
+            .withArguments(ScoreHolderArgument<StringArgument>("board"))
+            .executes(CommandExecutor { sender, args ->
+                if (sender.isOp) {
+                    if (args.size == 4) {
+                        val min = args[0] as Int
+                        val max = args[1] as Int
+                        val player = args[2] as String
+                        val board = Bukkit.getScoreboardManager()!!.mainScoreboard.getObjective(args[3] as String)!!
+
+                        board.getScore(player).score = (min..max).random()
+                    } else {
+                        sender.sendMessage("Too few arguments, /random <min> <max> <player> <board>")
+                    }
+                } else {
+                    sender.sendMessage(Color.color("&cNo permission!"))
+                }
+            })
     }
 }
